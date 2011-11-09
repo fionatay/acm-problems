bae33c17305010ca735282d5f0c4b57dabcd0526
Fiona Foo <mfoo@dahlia.cs.hmc.edu>
2011-11-08 17:55:24 -0800

    cowblank using binary search

--- cowblank.java
+++ cowblank.java
@@ -1,60 +1,84 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class cowblank {
-	private static int numCows;
-	private static int numBlankets;
-	
-	private static int[] cows;
-	private static int[] blankets;
-	
-	private static int[][] maxUnhappiness;
-	
-	public static void main(String [] args){
-		Scanner s = new Scanner(System.in);
-		numCows = s.nextInt();
-		numBlankets = s.nextInt();
-		
-		cows = new int[numCows];
-		for (int cow = 0; cow < numCows; cow++)
-			cows[cow] = s.nextInt();
-		Arrays.sort(cows);
-		
-		blankets = new int[numBlankets];
-		for (int blank = 0; blank < numBlankets; blank++)
-			blankets[blank] = s.nextInt();
-		Arrays.sort(blankets);
-		
-		maxUnhappiness = new int[numCows][numBlankets];
-		for (int i = 0; i < numCows; i++){
-			for (int j = 0; j < numBlankets; j++)
-				maxUnhappiness[i][j] = -1;
-		}
-		
-		for (int coldCows = 0; coldCows <= numCows; coldCows++) {
-			for (int blankets = 0; blankets <= numBlankets; blankets++) {
-				
-				
-			}
-		}
-	}
-	
-	private static int calculate(int cowsUsed, int blanketsUsed){
-		if (cowsUsed == numCows)
-			return 0;
-		else if (blanketsUsed == numBlankets)
-			return cows[cowsUsed] + calculate(cowsUsed + 1, blanketsUsed);
-		else {
-			if (maxUnhappiness[cowsUsed][blanketsUsed] != -1)
-				return maxUnhappiness[cowsUsed][blanketsUsed];
-			else {
-				int giveCow = Math.max(Math.abs(cows[cowsUsed] - blankets[blanketsUsed]), calculate(cowsUsed + 1, blanketsUsed + 1));
-				int nextCow = Math.max(cows[cowsUsed], calculate(cowsUsed + 1, blanketsUsed));
-				int nextBlankie = calculate(cowsUsed, blanketsUsed + 1);
-				int value = Math.min(giveCow, Math.min(nextCow, nextBlankie));
-				maxUnhappiness[cowsUsed][blanketsUsed] = value;
-				return value;
-			}
-		}
-	}
+    private static int numCows;
+    private static int numBlankets;
+
+    private static Integer[] cows;
+    private static Integer[] blankets;
+
+
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        numCows = s.nextInt();
+        numBlankets = s.nextInt();
+
+        cows = new Integer[numCows];
+        for (int cow = 0; cow < numCows; cow++)
+            cows[cow] = s.nextInt();
+        Arrays.sort(cows);
+
+        blankets = new Integer[numBlankets];
+        for (int blank = 0; blank < numBlankets; blank++)
+            blankets[blank] = s.nextInt();
+        Arrays.sort(blankets);
+
+        System.out.println("Cow coldnesses: " + Arrays.toString(cows));
+        System.out.println("Blankets available: " + Arrays.toString(blankets));
+        System.out.println(search());
+    }
+
+    public static int search(){
+        int lo = 0;
+        int hi = cows[numCows-1];
+        int lowest = hi;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi)/2;
+
+            // Mid is viable, so start searching below it
+            if (viable(mid)) {
+                lowest = mid;
+                hi = mid-1;
+            }
+
+            // Mid is not viable, so start searching slightly above it
+            else {
+                lo = mid+1;
+            }
+        }
+        return lowest;
+    }
+
+    public static boolean viable(int minColdness){
+        boolean viable = false;
+        ArrayList<Integer> remainingCows = new ArrayList<Integer>(
+                Arrays.asList(cows));
+        ArrayList<Integer> remainingBlankets = new ArrayList<Integer>(
+                Arrays.asList(blankets));
+
+        while (!remainingCows.isEmpty()) {
+            int first = remainingCows.get(0);
+
+            // Try to satisfy the cow without a blanket
+            if (first < minColdness)
+                remainingCows.remove(0);
+
+            // Try and find a blanket
+            else if (remainingBlankets.isEmpty())
+                return false;
+
+            // Try the first blanket
+            else {
+                int firstBlanket = remainingBlankets.get(0);
+                remainingBlankets.remove(0);
+                if (Math.abs(first-firstBlanket) <= minColdness)
+                    remainingCows.remove(0);
+            }
+        }
+        return true;
+    }
+
 }
